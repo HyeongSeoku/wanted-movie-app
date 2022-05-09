7f934d9943f1ad92ba5d510d50aad16636e676fc
HyeongSeoku <gudtjr3437@naver.com>
2022-05-10 02:47:16 +0900

    Feat : Set recoil & router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import './styles/index.scss'
+import Routes from './routes'
 import { RecoilRoot } from 'recoil'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <RecoilRoot>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </RecoilRoot>
+  <React.StrictMode>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </RecoilRoot>
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
