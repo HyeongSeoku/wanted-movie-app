0bfe2bd815d3c8874393a183c60db0f24a11e7d8
HyeongSeoku <gudtjr3437@naver.com>
2022-05-10 19:18:14 +0900

    Feat : DarkMode swtich component

---
+++ src/components/DarkMode/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './DarkMode.module.scss'
+import { useSetRecoilState } from 'recoil'
+import { themeMode } from '../../utils/atoms/atom'
+
+function DarkModeSwitch() {
+  const setIsDarkMode = useSetRecoilState(themeMode)
+
+  const handleDarkMode = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.currentTarget
+    if (checked) setIsDarkMode('dark')
+    else setIsDarkMode('light')
+  }
+
+  return (
+    <div className={styles.darkModeContainer}>
+      <input className={styles.checkBox} type='checkbox' id='switch' onChange={handleDarkMode} />
+      <label className={styles.checkBoxLabel} htmlFor='switch' />
+    </div>
+  )
+}
+
+export default DarkModeSwitch
