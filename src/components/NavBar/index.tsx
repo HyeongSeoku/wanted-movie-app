b7daf169546dfe90b0a9e353684206171790738e
HyeongSeoku <gudtjr3437@naver.com>
2022-05-10 19:13:38 +0900

    Feat : Navigation Bar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,33 @@
-import { navData } from '../../utils/constants/componentsData'
+import React from 'react'
+import { useRecoilState } from 'recoil'
+import cx from 'classnames'
+import { currentNav } from '../../utils/atoms/atom'
+import { NAV_DATA } from '../../utils/constants/componentsData'
 import styles from './NavBar.module.scss'
+import { Link, useNavigate } from 'react-router-dom'
 
 function NavBar() {
+  const [clickedtNav, setClickedNav] = useRecoilState<string>(currentNav)
+  const navigate = useNavigate()
+
+  const handleCurrentNav = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { nav } = e.currentTarget.dataset
+    if (nav !== undefined) setClickedNav(nav)
+    //  if (nav  === 'search_nav') navigate('/')
+    // else navigate('/bookmark')
+  }
+
   return (
     <footer className={styles.footer}>
       <nav>
         <ul className={styles.navList}>
-          {navData.map((i) => (
-            <li key={i.id} className={styles.navItem}>
-              {i.title}
+          {NAV_DATA.map((i) => (
+            <li key={i.id} className={cx(styles.navItem, { [styles.clicked]: clickedtNav === i.id })}>
+              <Link to={i.path}>
+                <button className={styles.navButton} type='button' data-nav={i.id} onClick={handleCurrentNav}>
+                  {i.icon}
+                </button>
+              </Link>
             </li>
           ))}
         </ul>
