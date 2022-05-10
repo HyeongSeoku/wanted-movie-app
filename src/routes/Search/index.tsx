cb31936f5d2ea43e0d6f1b77d66a48eaeff09a7f
HyeongSeoku <gudtjr3437@naver.com>
2022-05-10 19:22:34 +0900

    Feat : Route Page

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,12 +1,31 @@
 import styles from './Search.module.scss'
 import cx from 'classnames'
 import SearchInput from '../../components/SearchInput'
+import { useRecoilValue } from 'recoil'
+import { searchMovieData } from '../../utils/atoms/atom'
+import MovieCard from '../../components/MovieCard'
 
 function Search() {
+  const searchMovieList = useRecoilValue(searchMovieData)
+
   return (
     <div className={styles.searchSection}>
-      <h2>검색</h2>
-      <SearchInput />
+      <main className={styles.searchMain}>
+        <SearchInput />
+        <section className={styles.movieSection}>
+          {searchMovieList !== undefined &&
+            searchMovieList.map((movie) => (
+              <MovieCard
+                key={movie.imdbID}
+                Title={movie.Title}
+                Year={movie.Year}
+                imdbID={movie.imdbID}
+                Type={movie.Type}
+                Poster={movie.Poster}
+              />
+            ))}
+        </section>
+      </main>
     </div>
   )
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import styles from './Route.module.scss'
+import cx from 'classnames'
+import { Route, Routes } from 'react-router-dom'
+import Search from './Search'
+import BookMark from './BookMark'
+import NavBar from '../components/NavBar'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <Routes>
+            <Route path='/' element={<Search />} />
+            <Route path='/bookMark' element={<BookMark />} />
+          </Routes>
+        </div>
+        <NavBar />
+      </div>
+    </div>
+  )
+}
+
+export default App
