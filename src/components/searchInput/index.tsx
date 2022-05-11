27664c410a0505cac025f4b2214d9c5b90cb01ed
HyeongSeoku <gudtjr3437@naver.com>
2022-05-11 16:47:25 +0900

    Feat : Change submit event logic

--- src/components/searchInput/index.tsx
+++ src/components/searchInput/index.tsx
@@ -2,37 +2,54 @@ import styles from './searchInput.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useState } from 'react'
-import { useRecoilState } from 'recoil'
-import { searchMovieData } from '../../utils/atoms/atom'
+import { useRecoilState, useResetRecoilState, useSetRecoilState } from 'recoil'
+import { searchMovieData, searchPageNumber } from '../../utils/atoms/atom'
 import { moviesApi } from '../../utils/apis/api'
+
+// TODO : search validation 추가
 
 const SearchInput = () => {
   const [searchWord, setSearchWord] = useState<string>('')
-  const [pageNumber, setPageNumber] = useState<number>(1)
-  const [searchMovie, setSearchMovie] = useRecoilState(searchMovieData)
+
+  const [pageNumber, setPageNumber] = useRecoilState(searchPageNumber)
+  const setSearchList = useSetRecoilState(searchMovieData)
+
+  const resetSeachList = useResetRecoilState(searchMovieData)
+  const resetPageNumber = useResetRecoilState(searchPageNumber)
 
   useEffect(() => {
-    console.log(searchMovie)
-  }, [searchMovie])
+    if (pageNumber !== 0) {
+      callApi(searchWord, pageNumber)
+    }
+  }, [pageNumber])
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.currentTarget
-    setSearchWord(value)
-  }
-
-  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    // API 부분
-    moviesApi.searchMovie(searchWord, pageNumber).then((res) => {
+  function callApi(word: string, page: number) {
+    moviesApi.searchMovielist(word, page).then((res) => {
       const success = res.data.Response.toLowerCase()
-      if (success !== 'false' && res.data.Search !== undefined) {
-        setSearchMovie(res.data.Search)
+      const message = res.data.Error
+
+      if (success !== 'false') {
+        setSearchList(res.data.Search)
+      } else {
+        resetSeachList()
+        console.log(message)
       }
     })
   }
 
-  // 최하단 스크롤시 페이지 넘버 증가시키는 함수 추가
-  // setSearchMovie((prev) => [...prev, ...res.data.Search])
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget
+    setSearchWord(value)
+
+    // 검색어가 변화 했을경우 페이지 넘버를 초기화
+    if (pageNumber !== 0) resetPageNumber()
+  }
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // 검색시 항상 첫번째 페이지 결과를 받아 와야하므로
+    setPageNumber(1)
+  }
 
   return (
     <div className={styles.searchContainer}>
