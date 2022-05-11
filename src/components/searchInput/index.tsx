337c6e7daac23889f1feebaff0305e4f8627b297
HyeongSeoku <gudtjr3437@naver.com>
2022-05-11 22:09:53 +0900

    Feat : Api data deduplication & change key name

--- src/components/searchInput/index.tsx
+++ src/components/searchInput/index.tsx
@@ -1,24 +1,28 @@
+import * as _ from 'lodash'
 import styles from './searchInput.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useRecoilState, useResetRecoilState, useSetRecoilState } from 'recoil'
-import { searchMovieData, searchPageNumber } from '../../utils/atoms/atom'
+import { apiAdditionalData, currentSearchWord, searchMovieData, searchPageNumber } from '../../utils/atoms/atom'
 import { moviesApi } from '../../utils/apis/api'
+import { DATA_COUNT, FIRST_PAGE } from '../../utils/constants/standard'
+import { ApiResData, SearchModule } from '../../types/types.d'
 
 // TODO : search validation 추가
 
 const SearchInput = () => {
-  const [searchWord, setSearchWord] = useState<string>('')
-
+  const [searchWord, setSearchWord] = useRecoilState(currentSearchWord)
   const [pageNumber, setPageNumber] = useRecoilState(searchPageNumber)
+  const [additionalData, setAdditionalData] = useRecoilState(apiAdditionalData)
+
   const setSearchList = useSetRecoilState(searchMovieData)
 
   const resetSeachList = useResetRecoilState(searchMovieData)
   const resetPageNumber = useResetRecoilState(searchPageNumber)
 
   useEffect(() => {
-    if (pageNumber !== 0) {
+    if (pageNumber === FIRST_PAGE) {
       callApi(searchWord, pageNumber)
     }
   }, [pageNumber])
@@ -26,13 +30,33 @@ const SearchInput = () => {
   function callApi(word: string, page: number) {
     moviesApi.searchMovielist(word, page).then((res) => {
       const success = res.data.Response.toLowerCase()
+      /*
+        예외 처리
+        - too many results
+      */
       const message = res.data.Error
 
       if (success !== 'false') {
-        setSearchList(res.data.Search)
+        const totalResultCnt = Number(res.data.totalResults)
+        const lastPage = Math.ceil(totalResultCnt / DATA_COUNT)
+        const tempList: SearchModule.ISearchMovieList[] = []
+
+        res.data.Search.map((i: ApiResData.ISearchMovieData) =>
+          tempList.push({
+            title: i.Title,
+            year: i.Year.split('–').map((item: string) => Number(item)),
+            imdbID: i.imdbID,
+            type: i.Type,
+            poster: i.Poster,
+          })
+        )
+        // 중복제거
+        const newData = _.uniqBy(tempList, 'imdbID')
+
+        setSearchList(newData)
+        setAdditionalData({ totalResults: totalResultCnt, lastPageNumber: lastPage })
       } else {
         resetSeachList()
-        console.log(message)
       }
     })
   }
@@ -48,7 +72,7 @@ const SearchInput = () => {
   const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault()
     // 검색시 항상 첫번째 페이지 결과를 받아 와야하므로
-    setPageNumber(1)
+    setPageNumber(FIRST_PAGE)
   }
 
   return (
