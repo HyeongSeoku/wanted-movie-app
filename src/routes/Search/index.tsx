bbaee53cfd92079bab98472dcec6d9ab2f5ca0a5
HyeongSeoku <gudtjr3437@naver.com>
2022-05-11 22:12:08 +0900

    Feat : Infinite scroll

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,29 +1,90 @@
+import React, { useEffect } from 'react'
+import * as _ from 'lodash'
 import styles from './Search.module.scss'
 import cx from 'classnames'
 import SearchInput from '../../components/searchInput'
-import { useRecoilValue } from 'recoil'
-import { searchMovieData } from '../../utils/atoms/atom'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { currentSearchWord, searchMovieData, searchPageNumber } from '../../utils/atoms/atom'
 import MovieCard from '../../components/movieCard'
+import { moviesApi } from '../../utils/apis/api'
+import { DATA_COUNT, FIRST_PAGE } from '../../utils/constants/standard'
+import { useInView } from 'react-intersection-observer'
+import { ApiResData, SearchModule } from '../../types/types.d'
 
-const Search = () => {
-  const searchMovieList = useRecoilValue(searchMovieData)
+// searchMovieList.length === 0 이면 검색결과가 없습니다 노출
+const Search = (): JSX.Element => {
+  const [searchMovieList, setSearchMovieList] = useRecoilState(searchMovieData)
+  const currentWord = useRecoilValue(currentSearchWord)
+  const [pageNumber, setPageNumber] = useRecoilState(searchPageNumber)
+  const [ref, inView] = useInView()
+
+  // useEffect 페이지 증가 감지 => api  실행
+  useEffect(() => {
+    if (pageNumber !== FIRST_PAGE) {
+      callMoreApiData(pageNumber)
+    }
+  }, [pageNumber])
+
+  useEffect(() => {
+    if (inView) setPageNumber((prev) => prev + 1)
+  }, [inView])
+
+  // setPageNumber((prev) => prev + 1)
+
+  function callMoreApiData(page: number) {
+    moviesApi.searchMovielist(currentWord, page).then((res) => {
+      const dataList = res.data.Search
+      let tmpList: SearchModule.ISearchMovieList[] = []
+      if (dataList !== undefined) {
+        dataList.map((i: ApiResData.ISearchMovieData) =>
+          tmpList.push({
+            title: i.Title,
+            year: i.Year.split('–').map((item: string) => Number(item)),
+            imdbID: i.imdbID,
+            type: i.Type,
+            poster: i.Poster,
+          })
+        )
+
+        tmpList = searchMovieList.concat(tmpList)
+        tmpList = _.uniqBy(tmpList, 'imdbID')
+        setSearchMovieList(tmpList)
+      }
+    })
+  }
 
   return (
     <div className={styles.searchSection}>
       <main className={styles.searchMain}>
         <SearchInput />
         <section className={styles.movieSection}>
-          {searchMovieList !== undefined &&
-            searchMovieList.map((movie) => (
-              <MovieCard
-                key={movie.imdbID}
-                Title={movie.Title}
-                Year={movie.Year}
-                imdbID={movie.imdbID}
-                Type={movie.Type}
-                Poster={movie.Poster}
-              />
-            ))}
+          {searchMovieList.length !== 0 ? (
+            searchMovieList.map((movie, idx) => (
+              <React.Fragment key={movie.imdbID}>
+                {searchMovieList.length - 1 === idx ? (
+                  <div ref={ref}>
+                    <MovieCard
+                      title={movie.title}
+                      year={movie.year}
+                      imdbID={movie.imdbID}
+                      type={movie.type}
+                      poster={movie.poster}
+                    />
+                  </div>
+                ) : (
+                  <MovieCard
+                    title={movie.title}
+                    year={movie.year}
+                    imdbID={movie.imdbID}
+                    type={movie.type}
+                    poster={movie.poster}
+                  />
+                )}
+              </React.Fragment>
+            ))
+          ) : (
+            <div>검색 결과가 없습니다. </div>
+          )}
         </section>
       </main>
     </div>
