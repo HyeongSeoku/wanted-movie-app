f949cc7fde0069ac3d2378cb53d1e09e253c6b92
HyeongSeoku <gudtjr3437@naver.com>
2022-05-15 02:28:25 +0900

    Feat : Add book mark delete function

--- src/components/bookMarkCard/index.tsx
+++ src/components/bookMarkCard/index.tsx
@@ -1,13 +1,35 @@
 import styles from './bookMarkCard.module.scss'
 import { BookMarkModule } from '../../types/types.d'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { useRecoilState } from 'recoil'
+import { bookMarkList, searchMovieData } from '../../utils/atoms/atom'
+import { BOOKMARKLIST } from '../../utils/constants/componentsData'
 
 const BookMarkCard = ({ title, year, imdbID, type, poster }: BookMarkModule.IBookMarkModule) => {
+  const [bookMarkData, setBookMarkData] = useRecoilState<BookMarkModule.IBookMarkModule[]>(bookMarkList)
+  const [searchMovieList, setSearchMovieList] = useRecoilState(searchMovieData)
+
+  useEffect(() => {
+    localStorage.setItem(BOOKMARKLIST, JSON.stringify(bookMarkData))
+  }, [bookMarkData])
+
+  const deleteBookMark = () => {
+    if (window.confirm('즐겨찾기에서 제거 할까요')) {
+      setBookMarkData((prev) => prev.filter((movie) => movie.imdbID !== imdbID))
+      setSearchMovieList((prev) => prev.map((i) => (i.imdbID === imdbID ? { ...i, bookMark: false } : i)))
+      alert('제거 되었습니다.')
+    }
+  }
   return (
     <article className={styles.bookMarkCardContainer}>
       <div role='button' tabIndex={0} className={styles.bookMarkCardContents}>
         <img className={styles.poster} src={poster} alt={`${title}_이미지`} />
         <div className={styles.contentContainer}>
+          <div className={styles.iconContainer}>
+            <FontAwesomeIcon icon={faTrash} onClick={deleteBookMark} />
+          </div>
           <span className={styles.movieTitle}>
             <strong>{title}</strong>
           </span>
