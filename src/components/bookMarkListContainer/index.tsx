33b5c54b345646839888c6f056a3353c3a5e9c94
HyeongSeoku <gudtjr3437@naver.com>
2022-05-15 14:18:21 +0900

    Feat : Add draggalbe on book mark list's items

---
+++ src/components/bookMarkListContainer/index.tsx
@@ -0,0 +1,56 @@
+import styles from './bookMarkListContainer.module.scss'
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd'
+import { useRecoilState } from 'recoil'
+import { BookMarkModule } from '../../types/types.d'
+import _ from 'lodash'
+import { bookMarkList } from '../../utils/atoms/atom'
+import BookMarkCard from '../bookMarkCard'
+
+const BookMarkListConatiner = () => {
+  const [bookMarkData, setBookMarkData] = useRecoilState(bookMarkList)
+
+  const handleDragEnd = (r: DropResult) => {
+    console.log('emform')
+
+    const tempBookMark = _.cloneDeep(bookMarkData)
+    const bookMarkArrSrc = tempBookMark.splice(r.source.index, 1)[0]
+
+    tempBookMark.splice(r.destination?.index ?? 0, 0, bookMarkArrSrc)
+    setBookMarkData(tempBookMark)
+  }
+
+  return (
+    <div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId='bookMarkData'>
+          {(droppableProv) => (
+            <ul {...droppableProv.droppableProps} ref={droppableProv.innerRef}>
+              {bookMarkData.map((item: BookMarkModule.IBookMarkCardData, idx: number) => (
+                <Draggable key={`book_mark${item.imdbID}`} draggableId={`book_mark${item.imdbID}`} index={idx}>
+                  {(draggableProv) => (
+                    <li
+                      className={styles.bookMarkCard}
+                      ref={draggableProv.innerRef}
+                      {...draggableProv.dragHandleProps}
+                      {...draggableProv.draggableProps}
+                    >
+                      <BookMarkCard
+                        title={item.title}
+                        year={item.year}
+                        imdbID={item.imdbID}
+                        type={item.type}
+                        poster={item.poster}
+                      />
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  )
+}
+
+export default BookMarkListConatiner
