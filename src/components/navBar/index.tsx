d6c7a248a4e642bb4111390b9be499ba69d59b29
HyeongSeoku <gudtjr3437@naver.com>
2022-05-16 00:11:55 +0900

    Refactor : NavLink change

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,33 +1,23 @@
 import React from 'react'
-import { useRecoilState } from 'recoil'
 import cx from 'classnames'
-import { currentNav } from '../../utils/atoms/atom'
 import { NAV_DATA } from '../../utils/constants/componentsData'
 import styles from './navBar.module.scss'
-import { Link, useNavigate } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const NavBar = (): JSX.Element => {
-  const [clickedtNav, setClickedNav] = useRecoilState<string>(currentNav)
-  const navigate = useNavigate()
-
-  const handleCurrentNav = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const { nav } = e.currentTarget.dataset
-    if (nav !== undefined) setClickedNav(nav)
-    //  if (nav  === 'search_nav') navigate('/')
-    // else navigate('/bookmark')
-  }
+  const location = useLocation()
 
   return (
     <footer className={styles.footer}>
       <nav className={styles.navContainer}>
         <ul className={styles.navList}>
           {NAV_DATA.map((i) => (
-            <li key={i.id} className={cx(styles.navItem, { [styles.clicked]: clickedtNav === i.id })}>
-              <Link to={i.path}>
-                <button className={styles.navButton} type='button' data-nav={i.id} onClick={handleCurrentNav}>
+            <li key={i.id} className={cx(styles.navItem, { [styles.clicked]: location.pathname === i.path })}>
+              <NavLink to={i.path}>
+                <button className={styles.navButton} type='button' data-nav={i.id}>
                   {i.icon}
                 </button>
-              </Link>
+              </NavLink>
             </li>
           ))}
         </ul>
