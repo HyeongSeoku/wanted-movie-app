cddd59119a0d416b596b9d5d177fb1bb722709e5
HyeongSeoku <gudtjr3437@naver.com>
2022-05-15 14:45:44 +0900

    Delete : Remove strict mode to reslove react-beautiful-dnd error

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,11 @@ import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </RecoilRoot>
 )
 
 // If you want to start measuring performance in your app, pass a function
