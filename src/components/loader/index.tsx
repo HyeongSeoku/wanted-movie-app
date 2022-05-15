02ebbc46abf0e76343f2d7f9997ba5d12f90642c
HyeongSeoku <gudtjr3437@naver.com>
2022-05-15 14:19:11 +0900

    Style : Loader component styling

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -3,7 +3,11 @@ import styles from './loader.module.scss'
 const Loader = () => {
   return (
     <div className={styles.loaderContainer}>
-      <span className={styles.loaderText}>로더</span>
+      <div className={styles.spinner}>
+        <span className={styles.spinnerInner1} />
+        <span className={styles.spinnerInner2} />
+        <span className={styles.spinnerInner3} />
+      </div>
     </div>
   )
 }
