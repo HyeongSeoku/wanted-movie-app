7673a0ce0e01c924a8762c21177e2dcb58225418
HyeongSeoku <gudtjr3437@naver.com>
2022-05-15 14:21:35 +0900

    Feat : Add draggable on Book Mark List

--- src/routes/BookMark/index.tsx
+++ src/routes/BookMark/index.tsx
@@ -1,24 +1,16 @@
 import styles from './BookMark.module.scss'
 import cx from 'classnames'
-import { useRecoilState } from 'recoil'
-import { bookMarkList } from '../../utils/atoms/atom'
 import BookMarkCard from '../../components/bookMarkCard'
+import { DragDropContext } from 'react-beautiful-dnd'
+import BookMarkListConatiner from '../../components/bookMarkListContainer'
 
 const BookMark = () => {
-  const [bookMarkData, setBookMarkData] = useRecoilState(bookMarkList)
   return (
     <div>
       <h2>내 즐겨찾기</h2>
-      {bookMarkData.map((data) => (
-        <BookMarkCard
-          key={`bookMark_${data.imdbID}`}
-          title={data.title}
-          year={data.year}
-          imdbID={data.imdbID}
-          type={data.type}
-          poster={data.poster}
-        />
-      ))}
+      <main>
+        <BookMarkListConatiner />
+      </main>
     </div>
   )
 }
