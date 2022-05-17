f18fd89a46fb5eb9fde6eeb10cd67c1f295c75d5
HyeongSeoku <gudtjr3437@naver.com>
2022-05-18 02:33:42 +0900

    Feat : Modal portal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -9,14 +9,22 @@ import { faStar as faEmptyStar } from '@fortawesome/free-regular-svg-icons'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { BOOKMARKLIST } from 'utils/constants/componentsData'
 import { setLocalStorage } from 'service/store'
+import reactDom from 'react-dom'
 
 interface IModal {
   movie: MovieListData.IMovieList
 }
-
+interface IPortal {
+  children?: React.ReactNode
+}
 // TODO : esc 눌렀을때 모달 닫히도록 동작
 
 const Modal = ({ movie }: IModal) => {
+  const ModalPortal = ({ children }: IPortal) => {
+    const el = document.getElementById('modal')
+    return reactDom.createPortal(children, el!)
+  }
+
   const { title, year, imdbID, type, poster } = movie
   const [bookMarkMovieList, setBookMarkMovieList] = useRecoilState(bookMarkList)
   const [isModalOpen, setIsModalOpen] = useRecoilState(modalOpen)
@@ -56,44 +64,46 @@ const Modal = ({ movie }: IModal) => {
   }
 
   return (
-    <section
-      role='button'
-      tabIndex={0}
-      ref={backDropRef}
-      className={cx(styles.modalWrapper, { [styles.open]: isModalOpen })}
-      onClick={handleModalData}
-    >
-      <section className={styles.modalContent}>
-        <header className={styles.modalHeader}>
-          <button className={styles.closeBtn} type='button' onClick={closeModal}>
-            <span className={styles.closeBtnText}>X</span>
-          </button>
-        </header>
-        <main className={styles.contentMain}>
-          <div className={styles.posterContainer}>
-            <img className={styles.posterImg} src={poster} alt={`${title} 이미지`} />
-          </div>
-          <div className={styles.iconContainer}>
-            <span className={styles.type}>{type}</span>
-            <FontAwesomeIcon
-              icon={isBookMark() ? faStar : faEmptyStar}
-              className={cx(styles.icon, { [styles.isBookMarked]: isBookMark() })}
-              onClick={handleBookMark}
-            />
-          </div>
+    <ModalPortal>
+      <section
+        role='button'
+        tabIndex={0}
+        ref={backDropRef}
+        className={cx(styles.modalWrapper, { [styles.open]: isModalOpen })}
+        onClick={handleModalData}
+      >
+        <section className={styles.modalContent}>
+          <header className={styles.modalHeader}>
+            <button className={styles.closeBtn} type='button' onClick={closeModal}>
+              <span className={styles.closeBtnText}>X</span>
+            </button>
+          </header>
+          <main className={styles.contentMain}>
+            <div className={styles.posterContainer}>
+              <img className={styles.posterImg} src={poster} alt={`${title} 이미지`} />
+            </div>
+            <div className={styles.iconContainer}>
+              <span className={styles.type}>{type}</span>
+              <FontAwesomeIcon
+                icon={isBookMark() ? faStar : faEmptyStar}
+                className={cx(styles.icon, { [styles.isBookMarked]: isBookMark() })}
+                onClick={handleBookMark}
+              />
+            </div>
 
-          <div className={styles.title}>
-            <span>{title}</span>
-          </div>
-          <div className={styles.yearContainer}>
-            <span>{year}</span>
-          </div>
-          <div className={styles.imdbId}>{imdbID}</div>
+            <div className={styles.title}>
+              <span>{title}</span>
+            </div>
+            <div className={styles.yearContainer}>
+              <span>{year}</span>
+            </div>
+            <div className={styles.imdbId}>{imdbID}</div>
 
-          <div />
-        </main>
+            <div />
+          </main>
+        </section>
       </section>
-    </section>
+    </ModalPortal>
   )
 }
 
